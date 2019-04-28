62d1f8a39fe0aa045735e3ee305d6db690974256
linshunwei <2236650223@qq.com>
2019-04-28 11:17:22 +0800

    初始化

---
+++ tests/ImTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+class SimpleTest extends TestCase
+{
+	/**
+	 * @throws \Throwable
+	 */
+	public function testSimple()
+	{
+
+//		$builder = SearchInterfaceModel::search('te')->fuzzy()->range('id',1,200);
+//		$result = $builder->get();
+//		print_r($result);
+		$this->assertTrue(true);
+	}
+}
